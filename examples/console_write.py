60fc2c79b9374db0b532887d4bc3b4633e160916
magniff <artcollect@inbox.ru>
2015-03-21 19:47:00 +0300

    Fix examples.

--- examples/console_write.py
+++ examples/console_write.py
@@ -1,7 +1,7 @@
-from copper import IteratorBasedSource, Apply, Filter, OutLines, mainloop
+from copper import IteratorBasedSource, Apply, Filter, OutLines, StdOut, mainloop
 
 
 source = IteratorBasedSource(range(100))
-source >> Apply(lambda x: x**2+1) >> Filter(lambda x: x % 7) >> OutLines()
+source >> Apply(lambda x: x**2+1) >> Filter(lambda x: x % 7) >> OutLines(StdOut())
 
 mainloop.run(source)
--- examples/simple_numbers.py
+++ examples/simple_numbers.py
@@ -1,7 +1,7 @@
-from copper import IteratorBasedSource, Apply, OutLines, mainloop
+from copper import IteratorBasedSource, Apply, OutLines, StdOut, mainloop
 
 
 source = IteratorBasedSource(range(1, 4))
-source >> Apply(lambda x: x**2) >> OutLines()
+source >> Apply(lambda x: x**2) >> OutLines(StdOut())
 
 mainloop.run(source)
